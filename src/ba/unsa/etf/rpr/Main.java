63f25ea0424ac2c5a85d58973fe18fc4580a656e
dbalta1 <dbalta1@etf.unsa.ba>
2021-10-17 19:13:17 +0200

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Dobro došli na RPR");
+        System.out.println("cao");
+        System.out.println("opet cao");
+	// write your code here
+    }
+}
